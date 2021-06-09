086299730efa32c1a03d238dbd675a648b97790c
ejkim-dev <57036748+ejkim-dev@users.noreply.github.com>
2021-06-09 15:14:59 +0900

    [update] navbar 세션별로 이동하기

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY); 
-    console.log(`navbarHeight: ${navbarHeight}`);
+    // console.log(window.scrollY); 
+    // console.log(`navbarHeight: ${navbarHeight}`);
 
     // window scrollY가 navbarHeight보다 크면 navbar가 어두워지게
     if (window.scrollY > navbarHeight) {
@@ -14,4 +14,17 @@ document.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('navbar__dark');
     }
+});
+
+// navbar 메뉴 탭 할 시, 스크롤 다루기
+const  navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
